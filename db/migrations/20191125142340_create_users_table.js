1d9366a28fc201b6e1adcbed5b4f4d3bd541fddb
Dedekind561 <mitch@northcoders.com>
2019-11-25 14:24:22 +0000

    added users table migration file

---
+++ db/migrations/20191125142340_create_users_table.js
@@ -0,0 +1,11 @@
+exports.up = knex => {
+  return knex.schema.createTable('users', usersTable => {
+    usersTable.string('username').primary();
+    usersTable.string('avatar_url').notNullable();
+    usersTable.string('name').notNullable();
+  });
+};
+
+exports.down = knex => {
+  return knex.schema.dropTable('users');
+};
